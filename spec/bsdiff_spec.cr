12ff52b75bc31702617d510f44d880c5c5c68a76
Billy.Zheng <vil963@gmail.com>
2025-02-17 00:08:25 +0800

    Add more spec

--- spec/bsdiff_spec.cr
+++ spec/bsdiff_spec.cr
@@ -9,7 +9,14 @@ describe Bsdiff do
 
   it "work" do
     folder = "#{__DIR__}/files"
+    # Generating a patch for diff of foo_new.gz and foo_old.gz
     Bsdiff.bsdiff("#{folder}/foo_old.gz", "#{folder}/foo_new.gz", "#{folder}/foo.patch")
     File.exists?("#{folder}/foo.patch").should eq true
+
+    # Appling the patch foo.patch into the foo_old.gz, get updated_foo.gz
+    Bspatch.bspatch("#{folder}/foo_old.gz", "#{folder}/updated_foo.gz", "#{folder}/foo.patch")
+
+    # Check foo_new.gz same as updated_foo.gz
+    system("diff #{folder}/foo_new.gz #{folder}/updated_foo.gz").should eq true
   end
 end
--- src/bspatch/lib.cr
+++ src/bspatch/lib.cr
@@ -1,5 +1,5 @@
 @[Link(ldflags: "`command -v pkg-config > /dev/null && pkg-config --cflags --libs bzip2`")]
-@[Link(ldflags: "#{__DIR__}/../../ext/bspatch.a")]
+@[Link(ldflags: "#{__DIR__}/../../ext/bsdiff.a")]
 
 lib LibBspatch
   fun bspatch(x0 : LibC::Char*, x1 : LibC::Char*, x2 : LibC::Char*) : LibC::Char*
